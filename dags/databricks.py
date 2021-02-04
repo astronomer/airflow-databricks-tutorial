caaef734d672f6cb5b364d24df7cf54c4b67fa4b
Kenten Danas <kentendanas@kentens-mbp.lan>
2021-02-04 10:38:30 -0700

    Add databricks DAG

---
+++ dags/databricks.py
@@ -0,0 +1,51 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator, DatabricksRunNowOperator
+from datetime import datetime, timedelta 
+
+#Define params for Submit Run Operator
+new_cluster = {
+    'spark_version': '7.3.x-scala2.12',
+    'num_workers': 2,
+    'node_type_id': 'i3.xlarge',
+}
+
+notebook_task = {
+    'notebook_path': '/Users/kenten+001@astronomer.io/Quickstart_Notebook',
+}
+
+#Define params for Run Now Operator
+notebook_params = {
+    "Variable":5
+}
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2)
+}
+
+with DAG('databricks_dag',
+    start_date=datetime(2021, 1, 1),
+    schedule_interval='@daily',
+    catchup=False,
+    default_args=default_args
+    ) as dag:
+
+    opr_submit_run = DatabricksSubmitRunOperator(
+        task_id='submit_run',
+        databricks_conn_id='databricks',
+        new_cluster=new_cluster,
+        notebook_task=notebook_task
+    )
+    opr_run_now = DatabricksRunNowOperator(
+        task_id='run_now',
+        databricks_conn_id='databricks',
+        job_id=5,
+        notebook_params=notebook_params
+    )
+
+    opr_submit_run >> opr_run_now
