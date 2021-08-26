0ca53380898299b41bad094e98c4e185c176d793
Josh Fell <josh.d.fell@astronomer.io>
2021-08-26 10:32:03 -0400

    Refactoring default_args pattern

--- dags/databricks.py
+++ dags/databricks.py
@@ -20,21 +20,17 @@ notebook_task = {
 notebook_params = {"Variable": 5}
 
 
-default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "retries": 1,
-    "retry_delay": timedelta(minutes=2),
-}
-
 with DAG(
     "databricks_dag",
     start_date=datetime(2021, 1, 1),
     schedule_interval="@daily",
     catchup=False,
-    default_args=default_args,
+    default_args={
+        "email_on_failure": False,
+        "email_on_retry": False,
+        "retries": 1,
+        "retry_delay": timedelta(minutes=2),
+    },
 ) as dag:
 
     opr_submit_run = DatabricksSubmitRunOperator(
